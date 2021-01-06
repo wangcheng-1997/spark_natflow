db18e059a46e1b210acf97b402be9741bf174936
wang <425845843@qq.com>
2021-01-06 13:40:18 +0800

    用户活跃统计写入es

--- src/main/scala/com/zjtuojing/natflow/LivenessRpt.scala
+++ src/main/scala/com/zjtuojing/natflow/LivenessRpt.scala
@@ -2,6 +2,7 @@ package com.zjtuojing.natflow
 
 import java.text.SimpleDateFormat
 import java.util.Date
+import org.elasticsearch.spark.sql._
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
@@ -9,18 +10,19 @@ import org.apache.spark.sql.SparkSession
 object LivenessRpt {
   def main(args: Array[String]): Unit = {
     val properties = MyUtils.loadConf()
-    val date = new SimpleDateFormat("yyyyMMdd").format(new Date())
+    val datetime = MyUtils.getTaskTime._3._2
+    val date = new SimpleDateFormat("yyyyMMdd").format(datetime * 1000)
 
     val conf = new SparkConf()
       .setAppName(this.getClass.getSimpleName)
-            .setMaster("local[*]")
+      .setMaster("local[*]")
       .set("es.port", properties.getProperty("es.port"))
       .set("es.nodes", properties.getProperty("es.nodes"))
       .set("es.nodes.wan.only", properties.getProperty("es.nodes.wan.only"))
-      .set("spark.speculation", "true")
-      .set("spark.speculation.interval", "5000")
-      .set("spark.speculation.quantile", "0.9")
-      .set("spark.speculation.multiplier", "2")
+      .set("spark.speculation", "false")
+//      .set("spark.speculation.interval", "5000")
+//      .set("spark.speculation.quantile", "0.9")
+//      .set("spark.speculation.multiplier", "2")
       .set("spark.locality.wait", "10")
       .set("spark.storage.memoryFraction", "0.3")
 
@@ -46,11 +48,13 @@ object LivenessRpt {
       .map((_, 1))
       .reduceByKey(_ + _)
       .sortBy(_._2, false, 1)
+      .map(per => (per._1, per._2, datetime))
 
     import spark.implicits._
 
-    value.toDF("username","sum")
-      .show
+    value.toDF("username", "sum", "accesstime").saveToEs(s"/nat_liveness_$date/nat")
+
+
 
   }
 }
