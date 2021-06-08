9e378e6f775b271fb58a59638ab16f6cabddcde8
wang <425845843@qq.com>
2021-06-08 16:28:07 +0800

    用户活跃后续

---
+++ src/main/scala/com/zjtuojing/utils/JedisPoolSentine.scala
@@ -0,0 +1,75 @@
+package com.zjtuojing.utils
+
+import java.util
+
+import redis.clients.jedis.{Jedis, JedisPoolConfig, JedisSentinelPool}
+
+object JedisPoolSentine {
+
+  val properties = MyUtils.loadConf()
+
+  val MASTER_NAME = "mymaster"
+  val TIME_OUT = 30000
+
+  def getJedisPool(): JedisSentinelPool = {
+
+    val jedisPoolConfig = new JedisPoolConfig()
+    jedisPoolConfig.setTestOnBorrow(true)
+    jedisPoolConfig.setTestOnReturn(true)
+    jedisPoolConfig.setTestWhileIdle(true)
+    jedisPoolConfig.setMaxTotal(-1)
+
+    val sentinelSet = new util.HashSet[String]()
+    val redisUrl = properties.getProperty("redis.url")
+    val urls: Array[String] = redisUrl.split(",")
+    for (i <- urls.indices) {
+      sentinelSet.add(urls(i))
+    }
+
+    val password = properties.getProperty("redis.password")
+
+    val pool = new JedisSentinelPool(MASTER_NAME, sentinelSet, jedisPoolConfig, TIME_OUT, password)
+
+    pool
+  }
+
+  def getJedisClient(pool: JedisSentinelPool): Jedis = {
+    val jedis = pool.getResource
+    jedis
+  }
+
+  def getJedisClient(): Jedis = {
+    val jedisPoolConfig = new JedisPoolConfig()
+    jedisPoolConfig.setTestOnBorrow(true)
+    jedisPoolConfig.setTestOnReturn(true)
+    jedisPoolConfig.setTestWhileIdle(true)
+    jedisPoolConfig.setMaxTotal(-1)
+
+    val sentinelSet = new util.HashSet[String]()
+    val redisUrl = properties.getProperty("redis.url")
+    val urls: Array[String] = redisUrl.split(",")
+    for (i <- urls.indices) {
+      sentinelSet.add(urls(i))
+    }
+
+    val password = properties.getProperty("redis.password")
+
+    val pool = new JedisSentinelPool(MASTER_NAME, sentinelSet, jedisPoolConfig, TIME_OUT, password)
+    val jedis = pool.getResource
+    jedis
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val jedisPool = getJedisPool()
+    val jedis = getJedisClient(jedisPool)
+    jedis.hgetAll("broadband:userinfo")
+      .values().toArray
+      .take(1).foreach(println(_))
+
+
+    jedis.close()
+  }
+
+
+}
